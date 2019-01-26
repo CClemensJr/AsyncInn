7d0fa6777e78c9551858dc5ae1ba5ebaf60b5fdf
Charles Clemens <cclemensjr@gmail.com>
2019-01-26 14:34:46 -0800

    Created hotel model

---
+++ AsyncInn/Models/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Hotel
+    {
+        // Model properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        // Navigation Properies
+        public ICollection<HotelRoom> HotelRooms { get; set; }
+    }
+}
