5ee38e9b1473b799e88e6ada345de4cdd7c81d5f
Charles Clemens <cclemensjr@gmail.com>
2019-01-26 15:00:07 -0800

    Created room amenities model

---
+++ AsyncInn/Models/RoomAmenities.cs
@@ -0,0 +1,13 @@
+﻿namespace AsyncInn.Models
+{
+    public class RoomAmenities
+    {
+        // Composite Keys
+        public int AmenitiesID { get; set; }
+        public int RoomID { get; set; }
+
+        // Navigational properties
+        public Amenities Amenities { get; set; }
+        public Room Room { get; set; }
+    }
+}
