820181690c1070e8a7213feb81b6e1dd3acee5a9
Charles Clemens <cclemensjr@gmail.com>
2019-01-26 14:55:39 -0800

    Created Room model and Layout enum

---
+++ AsyncInn/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        // Primary Key
+        public int ID { get; set; }
+
+        // Other properties
+        public string Name { get; set; }
+        public int Layout { get; set; }
+
+        // Navigation properties
+        public ICollection<HotelRoom> HotelRooms { get; set; }
+        public ICollection<RoomAmenities> RoomAmenities { get; set;}
+    }
+
+    public enum Layout
+    {
+        Studio = 0,
+        OneBedroom = 1,
+        TwoBedroom = 2
+    }
+}
