24858fd60934997e15ebf8d584cdf709c9539288
Charles Clemens <cclemensjr@gmail.com>
2019-01-26 15:03:51 -0800

    Created Amenities model

---
+++ AsyncInn/Models/Amenities.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AsyncInn.Models
+{
+    public class Amenities
+    {
+        // Primary Key
+        public int ID { get; set; }
+
+        // Other properties
+        public string Name { get; set; }
+
+        // Navigational properties
+        public ICollection<RoomAmenities> RoomAmenities { get; set; }
+    }
+}
