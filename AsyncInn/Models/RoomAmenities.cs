5f2df9ff1cffcf1f639c6623cca3981cfe52eb98
Charles Clemens <cclemensjr@gmail.com>
2019-01-28 10:48:08 -0800

    Added database annotations to RoomAmenities

--- AsyncInn/Models/RoomAmenities.cs
+++ AsyncInn/Models/RoomAmenities.cs
@@ -1,9 +1,17 @@
-﻿namespace AsyncInn.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsyncInn.Models
 {
     public class RoomAmenities
     {
         // Composite Keys
+        [Key]
+        [Column(Order = 1)]
         public int AmenitiesID { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
         public int RoomID { get; set; }
 
         // Navigational properties
