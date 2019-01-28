9238438581a2997b6ad11e899cba29d8b96fbcc5
Charles Clemens <cclemensjr@gmail.com>
2019-01-28 10:47:14 -0800

    Added data annotations to HotelRoom

--- AsyncInn/Models/HotelRoom.cs
+++ AsyncInn/Models/HotelRoom.cs
@@ -1,14 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AsyncInn.Models
 {
     public class HotelRoom
     {
         // Composite Keys
+        [Key]
+        [Column(Order=1)]
         public int HotelID { get; set; }
+
+        [Key]
+        [Column(Order=2)]
         public int RoomNumber { get; set; }
 
         // Foreign Key
+        [ForeignKey("Room")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal RoomID { get; set; }
 
