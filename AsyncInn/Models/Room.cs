fcfc54e735b64eb6bf091eba33f67ad395102b25
Charles Clemens <cclemensjr@gmail.com>
2019-01-28 10:28:12 -0800

    Added data annotations to room model

--- AsyncInn/Models/Room.cs
+++ AsyncInn/Models/Room.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AsyncInn.Models
 {
     public class Room
     {
         // Primary Key
+        [Key]
         public int ID { get; set; }
 
         // Other properties
+        [Required]
+        [MaxLength(250)]
+        [MinLength(6)]
         public string Name { get; set; }
+        [Required]
         public int Layout { get; set; }
 
         // Navigation properties
