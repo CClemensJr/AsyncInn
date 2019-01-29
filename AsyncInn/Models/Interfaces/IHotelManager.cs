ede583be5015a7f87ec6ba76be7243587cb27e4b
Charles Clemens <cclemensjr@gmail.com>
2019-01-29 11:03:43 -0800

    Created interface for hotel controller

---
+++ AsyncInn/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    public interface IHotelManager
+    {
+        // Create a Hotel
+        Task AddNewHotel(Hotel hotel);
+
+        // Get a Hotel/Hotels
+        Task<Hotel> GetHotelDetails(int id);
+        Task<IEnumerable<Hotel>> GetAllHotelDetails();
+
+        // Update a Hotel
+        void EditHotelDetails(Hotel hotel);
+
+        // Delete a Hotel
+        void DeleteHotel(int id);
+    }
+}
