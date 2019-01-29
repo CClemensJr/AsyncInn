b5464bdd7bc9106c6a2b301674401b44590215e8
Charles Clemens <cclemensjr@gmail.com>
2019-01-29 11:16:01 -0800

    Created Hotel management service

---
+++ AsyncInn/Models/Services/HotelManagementService.cs
@@ -0,0 +1,36 @@
+﻿using AsyncInn.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Services
+{
+    public class HotelManagementService : IHotelManager
+    {
+        public Task AddNewHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditHotelDetails(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Hotel>> GetAllHotelDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> GetHotelDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
