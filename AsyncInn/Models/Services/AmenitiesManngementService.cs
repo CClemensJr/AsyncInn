866bc598305d8a9783a16c00c0eaee630c74f0ab
Charles Clemens <cclemensjr@gmail.com>
2019-01-30 10:34:28 -0800

    created private _table property

--- AsyncInn/Models/Services/AmenitiesManngementService.cs
+++ AsyncInn/Models/Services/AmenitiesManngementService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,31 @@ namespace AsyncInn.Models.Services
 {
     public class AmenitiesManngementService : IAmenitiesManager
     {
+        private AsyncInnDbContext _table { get; }
+
+        public Task AddAmenity(Amenities amenities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAmenity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Amenities>> GetAmenities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenities> GetAmenity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAmenity(Amenities amenities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
