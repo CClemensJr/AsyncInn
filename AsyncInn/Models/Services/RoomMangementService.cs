46c12d05709e42126036a77dbb94ebb08a0fa8b2
Charles Clemens <cclemensjr@gmail.com>
2019-01-30 09:55:49 -0800

    Created new service for IRoomManager interface

---
+++ AsyncInn/Models/Services/RoomMangementService.cs
@@ -0,0 +1,36 @@
+﻿using AsyncInn.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Services
+{
+    public class RoomMangementService : IRoomManager
+    {
+        public Task AddNewRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditRoomDetails(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Room>> GetAllRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Room> GetRoomDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
