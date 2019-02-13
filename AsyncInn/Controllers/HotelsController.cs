dd9aa1d5f94fee7075148fcb3c85b6bfdc924017
CClemensJr <cclemensjr@gmail.com>
2019-02-13 08:37:46 -0800

    Updated constructor to user interface

--- AsyncInn/Controllers/HotelsController.cs
+++ AsyncInn/Controllers/HotelsController.cs
@@ -7,16 +7,17 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AsyncInn.Data;
 using AsyncInn.Models;
+using AsyncInn.Models.Interfaces;
 
 namespace AsyncInn.Controllers
 {
     public class HotelsController : Controller
     {
-        private readonly AsyncInnDbContext _context;
+        private readonly IHotelManager _hotel;
 
-        public HotelsController(AsyncInnDbContext context)
+        public HotelsController(IHotelManager hotel)
         {
-            _context = context;
+            _hotel = hotel;
         }
 
         // GET: Hotels
