9270b81eba51a9e896136d220873fa3f6dceaf18
CClemensJr <cclemensjr@gmail.com>
2019-02-12 22:28:52 -0800

    Added summary methods

--- AsyncInn/Models/Services/RoomManagerService.cs
+++ AsyncInn/Models/Services/RoomManagerService.cs
@@ -12,13 +12,21 @@ namespace AsyncInn.Models.Services
     {
         private AsyncInnDbContext _table { get; }
 
+        /// <summary>
+        /// A custom contructor that assigns a dbcontext to the property
+        /// </summary>
+        /// <param name="hotel"></param>
         public RoomManagerService(AsyncInnDbContext room)
         {
             _table = room;
         }
 
 
-
+        /// <summary>
+        /// The AddNewHotel method takes in a Hotel object and adds it to the database
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns>A Task object</returns>
         public async Task AddNewRoom(Room room)
         {
             _table.Rooms.Add(room);
@@ -26,6 +34,12 @@ namespace AsyncInn.Models.Services
             await _table.SaveChangesAsync();
         }
 
+
+        /// <summary>
+        /// Thus method deletes a hotel from the database if it exists and then saves the changes
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A Task object</returns>
         public async Task DeleteRoom(int id)
         {
             Room room = await _table.Rooms.FindAsync(id);
@@ -38,6 +52,12 @@ namespace AsyncInn.Models.Services
             }
         }
 
+
+        /// <summary>
+        /// This method updates the details of a Hotel if it exists in the database then saves the changes
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns>A Task object</returns>
         public async Task EditRoomDetails(Room room)
         {
             if (await _table.Rooms.FirstOrDefaultAsync(r => r.ID == room.ID) != null)
@@ -49,12 +69,21 @@ namespace AsyncInn.Models.Services
         }
 
 
+        /// <summary>
+        /// This method returns all of the Hotels in the database
+        /// </summary>
+        /// <returns>A Task object carrying a list of Hotel objects</returns>
         public async Task<IEnumerable<Room>> GetAllRooms()
         {
             return await _table.Rooms.ToListAsync();
         }
 
 
+        /// <summary>
+        /// This method provides the details of a specific hotel in the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A Task object containing a Hotel</returns>
         public async Task<Room> GetRoomDetails(int id)
         {
             return await _table.Rooms.FindAsync(id);
