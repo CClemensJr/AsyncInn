403912b89153150846bea1ec502a97205d768bff
CClemensJr <cclemensjr@gmail.com>
2019-02-12 22:13:44 -0800

    Added interface and autoimplemented methods

--- AsyncInn/Models/Services/RoomManagerService.cs
+++ AsyncInn/Models/Services/RoomManagerService.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using AsyncInn.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Models.Services
 {
-    public class RoomManagerService
+    public class RoomManagerService : IRoomManager
     {
+        public Task AddNewRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditRoomDetails(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Room>> GetAllRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Room> GetRoomDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
