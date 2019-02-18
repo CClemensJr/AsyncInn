b9c77771f7164f8bb04b9790417e5acf12c47438
CClemensJr <cclemensjr@gmail.com>
2019-02-17 17:42:27 -0800

    Added new migration

---
+++ AsyncInn/Migrations/20190218014115_AddedCounterPropertyToRoom.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class AddedCounterPropertyToRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfAmenities",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfAmenities",
+                table: "Rooms");
+        }
+    }
+}
