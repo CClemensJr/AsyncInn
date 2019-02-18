bc114a571d7bb8b0b69e5ee105bfe949ae7cae82
CClemensJr <cclemensjr@gmail.com>
2019-02-17 16:56:17 -0800

    Added room counter property

---
+++ AsyncInn/Migrations/20190218005408_AddedRoomCounter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class AddedRoomCounter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfRooms",
+                table: "Hotels",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfRooms",
+                table: "Hotels");
+        }
+    }
+}
