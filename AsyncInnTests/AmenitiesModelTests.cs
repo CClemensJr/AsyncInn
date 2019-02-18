b4c895374247a482b853425bb78b26edd6092468
CClemensJr <cclemensjr@gmail.com>
2019-02-17 20:35:42 -0800

    Added tests classes for each model

---
+++ AsyncInnTests/AmenitiesModelTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+
+namespace AsyncInnTests
+{
+    public class GetterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+
+    public class SetterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
---
+++ AsyncInnTests/HotelRoomModelTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncInnTests
+{
+    class HotelRoomModelTests
+    {
+    }
+}
